69a80a1e05a3afbfacee7e5454b17c5fbbcb1074
Romar8 <romarharwood@hotmail.com>
2024-01-22 13:17:52 -0500

    final

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,20 @@
+// Import necessary modules and libraries"use client";
 "use client";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Define the functional component named Home
 export default function Home() {
+  // State variables to manage component state
   const [currencies, setCurrencies] = useState([]);
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("USD");
   const [amount, setAmount] = useState("");
   const [convertedResult, setConvertedResult] = useState(null);
 
+  // useEffect to fetch currencies from the server on component mount
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/currencies")
@@ -18,9 +22,9 @@ export default function Home() {
       .catch((error) => console.error("Error fetching currencies", error));
   }, []);
 
+  // Function to convert currency when the "Convert" button is clicked
   const convertCurrency = () => {
     axios
-
       .get(
         `http://localhost:8000/api/convert/${fromCurrency}/${toCurrency}/${amount}`
       )
@@ -28,10 +32,12 @@ export default function Home() {
       .catch((error) => console.error("Error converting currency:", error));
   };
 
+  // Render the component with JSX
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
       <div className="bg-light py-5 border border-3 rounded-5 shadow-lg px-5 col-9">
         <h1 className="text center mb-5">Fx Currency Converter</h1>
+
         {/* Dropdown for selecting the source currency */}
         <div className="d-flex">
           <div className="col-6 px-3">
@@ -47,6 +53,7 @@ export default function Home() {
               ))}
             </select>
           </div>
+
           {/* Dropdown for selecting the target currency */}
           <div className="col-6 px-3">
             <h3>To</h3>
@@ -62,6 +69,7 @@ export default function Home() {
             </select>
           </div>
         </div>
+
         {/* Input field for entering the amount */}
         <div className="px-3 mt-3 mb-5">
           <h3>Amount</h3>
@@ -70,10 +78,14 @@ export default function Home() {
             className="form-control form-control-lg mb-3 "
             onChange={(e) => setAmount(e.target.value)}
           ></input>
+
+          {/* Button to trigger the currency conversion */}
           <button className="btn btn-success" onClick={convertCurrency}>
             Convert
           </button>
         </div>
+
+        {/* Display the result of the currency conversion */}
         {convertedResult && (
           <p className="fs-3">
             {" "}
